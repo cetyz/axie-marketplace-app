3c6308a43d13466267e27bc1a32662eb592d1ca9
Charles <36413703+cetyz@users.noreply.github.com>
2021-09-28 02:51:19 +0800

    updates

--- axie-app/scrape.py
+++ axie-app/scrape.py
@@ -32,33 +32,8 @@ def scrape():
             parts = list(parts)
         )
         
-        g.searched = True
         g.results = response
         
-        return redirect(url_for('scrape.results'))
+        return render_template('scrape.html', results=response)
     
     return render_template('scrape.html')
-
-@bp.route('/results', methods=(['GET', 'POST']))
-def results():
-    # if you would like to search...
-    if request.method == 'POST':  
-        # these are the fields you can fill in...
-        parts = request.form['parts']
-        classes = request.form['classes']
-        pureness = request.form['pureness']
-        breed_count = request.form['breed_count']
-        hp = request.form['hp']
-        skill = request.form['skill']
-        speed = request.form['speed']
-        morale = request.form['morale']
-        
-        response = get_axie_market_list(
-            parts = list(parts)
-        )
-        
-        g.results = response
-        
-        return redirect(url_for('scrape.results'))
-    
-    return render_template('results.html')
